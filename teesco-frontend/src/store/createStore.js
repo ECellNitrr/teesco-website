f9094012561df3de8100e379775aafebdcccf219
Pratik <pchy.393@gmail.com>
2020-05-18 17:27:06 +0530

    git hooks(husky) setup

--- teesco-frontend/src/store/createStore.js
+++ teesco-frontend/src/store/createStore.js
@@ -1,9 +1,9 @@
 import {createStore,applyMiddleware} from 'redux'
 import RootReducer from './reducers/rootReducer';
+import Logger from 'redux-logger'
 import ReduxThunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-export const middlewares =[ReduxThunk]
+export const middlewares =[ReduxThunk,Logger]
 
-export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
-
-export const store = createStoreWithMiddleware(RootReducer)
+export const store = createStore(RootReducer,composeWithDevTools(applyMiddleware(...middlewares)))
