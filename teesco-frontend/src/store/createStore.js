d2e08116e81c2d5510110e548b7a614f5dcd99da
naveennvrgup <naveennvrgup@gmail.com>
2020-05-23 11:48:45 +0530

    feat: Add PR template

--- teesco-frontend/src/store/createStore.js
+++
@@ -1,9 +0,0 @@
-import {createStore,applyMiddleware} from 'redux'
-import RootReducer from './reducers/rootReducer';
-import Logger from 'redux-logger'
-import ReduxThunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-
-export const middlewares =[ReduxThunk,Logger]
-
-export const store = createStore(RootReducer,composeWithDevTools(applyMiddleware(...middlewares)))
