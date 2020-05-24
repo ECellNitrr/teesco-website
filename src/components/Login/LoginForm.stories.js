119e74baaab7e7bb3ad5fdccde4ffd527c778cdb
naveennvrgup <naveennvrgup@gmail.com>
2020-05-24 23:57:06 +0530

    feat: Add state to knobs

--- src/components/Login/LoginForm.stories.js
+++ src/components/Login/LoginForm.stories.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import LoginForm from './LoginForm'
+import { withKnobs, object } from '@storybook/addon-knobs/react'
 
 
 // attaches component to the story book
 export default {
     component: LoginForm,
     title: 'LoginForm',
+    decorators: [withKnobs], // use to display state in storybook
     excludeStories: /.*Data$/, // export ending with Data wont be treated as a story
 }
 
@@ -43,7 +45,7 @@ const loadingStateData = {
 
 
 // creating the stories
-export const initialState = () => <LoginForm stateData={initialStateData} />
-export const errorState = () => <LoginForm stateData={errorStateData} />
-export const loadingState = () => <LoginForm stateData={loadingStateData} />
-export const beforeRequestState = () => <LoginForm stateData={beforeRequestStateData} />
+export const initialState = () => <LoginForm stateData={object('state',initialStateData)} />
+export const errorState = () => <LoginForm stateData={object('state',errorStateData)} />
+export const loadingState = () => <LoginForm stateData={object('state',loadingStateData)} />
+export const beforeRequestState = () => <LoginForm stateData={object('state',beforeRequestStateData)} />
