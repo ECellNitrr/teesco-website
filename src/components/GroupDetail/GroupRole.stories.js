b05e82950cb576ff31d10b62ae009f132af49dd0
Nikhil <nikhil13012000@gmail.com>
2020-05-27 09:29:35 +0530

    Create stories for GroupRole

---
+++ src/components/GroupDetail/GroupRole.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import GroupRole from './GroupRole';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
+
+export default {
+    component: GroupRole,
+    title: 'GroupRole',
+    decorators: [withKnobs],
+    excludeStories: /.*Data$/,
+};
+
+// Initial State of the story with the role
+export const initialStateData = {
+    role:
+        'Managing the sub-ordinates. Supervising the work in the respective domains. Interaction with other domains...',
+    loading: false,
+};
+
+//Loading state of the story with loading false
+export const loadingStateData = {
+    role: '',
+    loading: true,
+};
+
+//Creating stories
+export const initialState = () => (
+    <GroupRole stateData={object('state', initialStateData)} />
+);
+export const loadingState = () => (
+    <GroupRole stateData={object('state', loadingStateData)} />
+);
