e377caa4db0a3758376323d15f0fa5b97939ffd7
rajesh6161 <rajeshranjan2712000@gmail.com>
2020-05-30 17:08:27 +0530

    Added side drawer(beta-2)

--- src/components/SideDrawer/SideDrawer.jsx
+++ src/components/SideDrawer/SideDrawer.jsx
@@ -5,7 +5,7 @@ import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
-import ListSubheader from "@material-ui/core/ListSubheader";
+
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
@@ -19,6 +19,10 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 import Logo from "./logo.png";
 
@@ -57,10 +61,16 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
+  groups: {
+    marginTop: -30,
+    width: 275,
+  },
 });
 
-export default function SwipeableTemporaryDrawer() {
+export default function SwipeableTemporaryDrawer(props) {
   const classes = useStyles();
+  const { actions, orgs, organisations } = props.defaultData;
+  console.log(organisations);
   const [state, setState] = React.useState({
     left: false,
   });
@@ -97,51 +107,150 @@ export default function SwipeableTemporaryDrawer() {
 
       <Divider />
 
-      <Card className={classes.card} variant="outlined">
-        <CardContent>
-          <Typography className={classes.center} variant="h6" component="h2">
-            No Organizations Found
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Grid container spacing={3} justify="center">
-            <Grid item>
-              <Button>Create</Button>
+      {!orgs ? (
+        <Card className={classes.card} variant="outlined">
+          <CardContent>
+            <Typography className={classes.center} variant="h6" component="h2">
+              No Organisations Found
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Grid container spacing={3} justify="center">
+              <Grid item>
+                <Button>Create</Button>
+              </Grid>
+              <Grid item>
+                <Button>Join</Button>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Button>Join</Button>
-            </Grid>
-          </Grid>
-        </CardActions>
-      </Card>
+          </CardActions>
+        </Card>
+      ) : (
+        <Card className={classes.card} variant="outlined">
+          <CardContent>
+            <Typography className={classes.center} variant="h6" component="h2">
+              Organisations Found
+            </Typography>
+          </CardContent>
+          <div className="expansionPanel">
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography className={classes.heading}>
+                  {organisations.org1.name}
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <List
+                  component="nav"
+                  className={classes.groups}
+                  aria-label="org groups"
+                >
+                  <ListItem
+                    onClick={toggleDrawer(anchor, false)}
+                    onKeyDown={toggleDrawer(anchor, false)}
+                    button
+                  >
+                    <ListItemText primary={organisations.org1.group1.name} />
+                  </ListItem>
+                </List>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+
+            {/* ---------------------------- Org-2 ---------------------------- */}
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography className={classes.heading}>
+                  {organisations.org2.name}
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <List
+                  component="nav"
+                  className={classes.groups}
+                  aria-label="org groups"
+                >
+                  <ListItem
+                    onClick={toggleDrawer(anchor, false)}
+                    onKeyDown={toggleDrawer(anchor, false)}
+                    button
+                  >
+                    <ListItemText primary={organisations.org2.group1.name} />
+                  </ListItem>
+                </List>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+            {/* ---------------------------- Org-3 ---------------------------- */}
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography className={classes.heading}>
+                  {organisations.org3.name}
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <List
+                  component="nav"
+                  className={classes.groups}
+                  aria-label="org groups"
+                >
+                  <ListItem
+                    onClick={toggleDrawer(anchor, false)}
+                    onKeyDown={toggleDrawer(anchor, false)}
+                    button
+                  >
+                    <ListItemText primary={organisations.org3.group1.name} />
+                  </ListItem>
+                </List>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </div>
+        </Card>
+      )}
 
       {/* ----------------- LIST ------------------------------ */}
       <List
         component="nav"
         aria-labelledby="nested-list-subheader"
-        subheader={
-          <ListSubheader component="div" id="nested-list-subheader">
-            Nested List Items
-          </ListSubheader>
-        }
         className={classes.root}
       >
         <ListItem button onClick={handleClick}>
-          <ListItemIcon>
+          {/* <ListItemIcon>
             <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Inbox" />
+          </ListItemIcon> */}
+          <ListItemText primary="Actions" />
           {open ? <ExpandLess /> : <ExpandMore />}
         </ListItem>
         <Collapse in={open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
-              <ListItemText primary="Starred" />
-            </ListItem>
-          </List>
+          {["Tasks", "People", "LB Settings", "Statistics", "Certificate"].map(
+            (el) => (
+              <List component="div" disablePadding>
+                <ListItem
+                  disabled={actions ? false : true}
+                  onClick={toggleDrawer(anchor, false)}
+                  onKeyDown={toggleDrawer(anchor, false)}
+                  button
+                  button
+                  className={classes.nested}
+                >
+                  <ListItemIcon>
+                    <StarBorder />
+                  </ListItemIcon>
+                  <ListItemText primary={el} />
+                </ListItem>
+              </List>
+            )
+          )}
         </Collapse>
       </List>
     </div>
@@ -151,7 +260,7 @@ export default function SwipeableTemporaryDrawer() {
     <div>
       {["left"].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Button onClick={toggleDrawer(anchor, true)}>Open</Button>
           <SwipeableDrawer
             anchor={anchor}
             open={state[anchor]}
--- src/components/SideDrawer/SideDrawer.stories.js
+++ src/components/SideDrawer/SideDrawer.stories.js
@@ -9,4 +9,61 @@ export default {
   excludeStories: /.*Data$/, // export ending with Data wont be treated as a story
 };
 
-export const Drawer = () => <SideDrawer />;
+const initialState = {
+  actions: false,
+  orgs: false,
+};
+
+// let orgTypes = {
+//   organisations: [
+//     (org1 = {
+//       orgName: "Org-1",
+//       g1: {
+//         groupName: "Group-1",
+//       },
+//     }),
+//     (org2 = {
+//       orgName: "Org-2",
+//       g2: {
+//         groupName: "Group-2",
+//       },
+//     }),
+//     (org3 = {
+//       orgName: "Org-3",
+//       g3: {
+//         groupName: "Group-3",
+//       },
+//     }),
+//   ],
+// };
+const finalState = {
+  actions: true,
+  orgs: true,
+  organisations: {
+    org1: {
+      name: "Robotix Club",
+      group1: {
+        name: "Group-1",
+      },
+    },
+    org2: {
+      name: "ECELL",
+      group1: {
+        name: "Group-1",
+      },
+    },
+    org3: {
+      name: "Innovation Cell",
+      group1: {
+        name: "Group-1",
+      },
+    },
+  },
+};
+
+export const DefaultDrawer = () => (
+  <SideDrawer defaultData={object("state", initialState)} />
+);
+export const UserActionDrawer = () => (
+  <SideDrawer defaultData={object("state", finalState)} />
+);
