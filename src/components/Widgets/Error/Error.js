3e4687c8037a26caad28c45a2dbac178ed893d72
Nikhil <nikhil13012000@gmail.com>
2020-06-03 11:39:50 +0530

    finish up ui

---
+++ src/components/Widgets/Error/Error.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Grid, Typography } from '@material-ui/core';
+//Icon
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+
+/*Component requires
+error => Error to be displayed(string)
+width => Total width of the box in % (string)
+direction => Direction of the grid {column, row} (string) */
+
+const Error = ({ stateData: { error, width, direction, iconSize } }) => {
+    return (
+        <Box
+            width={width}
+            style={{
+                margin: '20px auto',
+            }}>
+            <Grid
+                container
+                spacing={1}
+                direction={direction}
+                justify='center'
+                alignItems='center'>
+                <Grid item>
+                    <SentimentVeryDissatisfiedIcon
+                        color='secondary'
+                        style={{ fontSize: iconSize }}
+                    />
+                </Grid>
+                <Grid item>
+                    <Typography align='center' variant='h6'>
+                        <Box>{error}</Box>
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
+
+Error.propTypes = {
+    stateData: PropTypes.shape({
+        error: PropTypes.string.isRequired,
+        width: PropTypes.string.isRequired,
+        direction: PropTypes.string.isRequired,
+        iconSize: PropTypes.number.isRequired,
+    }),
+};
+
+export default Error;
