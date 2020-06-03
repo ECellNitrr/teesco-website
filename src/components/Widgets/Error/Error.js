da03978d803ac4cfbb27f127460aac40a82fddbe
Nikhil <nikhil13012000@gmail.com>
2020-06-03 12:19:36 +0530

    cleanup

--- src/components/Widgets/Error/Error.js
+++ src/components/Widgets/Error/Error.js
@@ -7,7 +7,8 @@ import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissa
 /*Component requires
 error => Error to be displayed(string)
 width => Total width of the box in % (string)
-direction => Direction of the grid {column, row} (string) */
+direction => Direction of the grid {column, row} (string) 
+iconSize => size of the SentimentVeryDissatisfiedIcon (number)*/
 
 const Error = ({ stateData: { error, width, direction, iconSize } }) => {
     return (
