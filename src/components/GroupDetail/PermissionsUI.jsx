2c9987530a78531e9d4bba21f64d381396042e8e
Naveen Sundar <naveennvrgup@gmail.com>
2020-06-15 14:48:57 +0530

    fix: Reverted unnecessary changes

--- src/components/GroupDetail/PermissionsUI.jsx
+++ src/components/GroupDetail/PermissionsUI.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
-import "./default.css";
 import Grid from "@material-ui/core/Grid";
 
 import Typography from "@material-ui/core/Typography";
