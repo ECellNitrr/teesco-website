1c3203a1d8a5986fa1c06f2e673ca04b158b97a8
rajesh6161 <rajeshranjan2712000@gmail.com>
2021-02-26 10:25:36 +0530

    sidedrawer made

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import './drawer.css';
+import './drawer.scss';
 import { getUserOrganisationsAction } from '../../actions/User';
 import { getOrgGroupsAction } from '../../actions/Organisation';
 
